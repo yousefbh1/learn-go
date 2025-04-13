78e5a265a9ad973f8ba88609ba60d2ccd8a07952
Yousef Thulqarnain <yousefbh@umich.edu>
2025-04-12 22:03:08 -0400

    update

--- project1/hello_test.go
+++ project1/hello_test.go
@@ -3,8 +3,20 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Me")
-	want := "Hello, Me"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("Me")
+		want := "Hello, Me"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("saying hello empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
